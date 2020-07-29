cd4f925561617f4bc990ef29d37fe3e803fd2c8b
DenisViskov <DenisViskov16@gmail.com>
2020-07-29 16:25:28 +0300

    3. Прерывание нити [#334688]

---
+++ src/main/java/interruptthread/ConsoleProgress.java
@@ -0,0 +1,34 @@
+package interruptthread;
+
+/**
+ * Class has example loading in another thread
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 29.07.2020
+ */
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                System.out.print("\r load: " + "\\");
+                Thread.sleep(500);
+                System.out.print("\r load: " + "|");
+                Thread.sleep(500);
+                System.out.print("\r load: " + "/");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1);
+        progress.interrupt();
+    }
+}
