dfd41e2132717cbbbfb7880b3c6b1fb011f44f91
DenisViskov <denisviskov16@gmail.com>
2020-07-30 16:09:34 +0300

    3. Immutable объекты [#334692]

---
+++ src/main/java/immutableobjects/Node.java
@@ -0,0 +1,37 @@
+package immutableobjects;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+/**
+ * Class has example immutable class
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 30.07.2020
+ */
+@Immutable
+public class Node<T> {
+    /**
+     * Next Node
+     */
+    private final Node next;
+
+    /**
+     * Value
+     */
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
+
