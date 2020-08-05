f710beedbcfbecd747fc1d88af942caacd9ca3fe
DenisViskov <denisviskov16@gmail.com>
2020-08-05 11:07:24 +0300

    2. Обеспечить остановку потребителя. [#334678]

---
+++ src/main/java/stopconsumer/ParallelSearch.java
@@ -0,0 +1,45 @@
+package stopconsumer;
+
+import net.jcip.annotations.ThreadSafe;
+import shabloneproducerconsumer.SimpleBlockingQueue;
+
+/**
+ * Класс реализует ...
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 05.08.2020
+ */
+@ThreadSafe
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            System.out.println("Consumer interrupted");
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
