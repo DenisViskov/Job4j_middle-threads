8c67e1f9e5a3a6a2646209583a16563e75c6df0d
DenisViskov <DenisViskov16@gmail.com>
2020-08-14 13:30:08 +0300

    2. Thread switcher [#334709]

---
+++ src/main/java/switcher/Switcher.java
@@ -0,0 +1,59 @@
+package switcher;
+
+/**
+ * Class is a Switcher
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 13.08.2020
+ */
+public class Switcher {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> {
+                    synchronized (Switcher.class) {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            System.out.println("Thread A");
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                        Switcher.class.notifyAll();
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    synchronized (Switcher.class) {
+                        if (!first.isInterrupted()) {
+                            try {
+                                Switcher.class.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                        while (true) {
+                            System.out.println("Thread B");
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                }
+        );
+        second.start();
+        first.start();
+        Thread.sleep(5000);
+        first.interrupt();
+        first.join();
+        second.join();
+    }
+}
