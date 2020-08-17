07daae87c71adcc8acea64ccf23aeeb23c444f64
DenisViskov <DenisViskov16@gmail.com>
2020-08-17 11:26:03 +0300

    2. Thread switcher [#334709] update

--- src/main/java/switcher/MasterSlaveBarrier.java
+++ src/main/java/switcher/MasterSlaveBarrier.java
@@ -1,5 +1,8 @@
 package switcher;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * Class is a Master Slave Barrier
  *
@@ -7,13 +10,21 @@ package switcher;
  * @version 1.0
  * @since 16.08.2020
  */
+@ThreadSafe
 public class MasterSlaveBarrier {
+
+    /**
+     * Current thread
+     */
+    @GuardedBy("this")
+    private Thread current;
 
     /**
      * Method of run master
      */
-    public void tryMaster() {
-        while (!Thread.currentThread().isInterrupted()) {
+    public synchronized void tryMaster() {
+        setCurrent(Thread.currentThread());
+        while (!current.isInterrupted()) {
             System.out.println("Thread A");
             try {
                 Thread.sleep(1000);
@@ -22,13 +33,23 @@ public class MasterSlaveBarrier {
                 Thread.currentThread().interrupt();
             }
         }
+        doneMaster();
     }
 
     /**
      * Method of run slave
      */
-    public void trySlave() {
-        while (!Thread.currentThread().isInterrupted()) {
+    public synchronized void trySlave() {
+        if (current == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+        setCurrent(Thread.currentThread());
+        while (!current.isInterrupted()) {
             System.out.println("Thread B");
             try {
                 Thread.sleep(1000);
@@ -37,6 +58,7 @@ public class MasterSlaveBarrier {
                 Thread.currentThread().interrupt();
             }
         }
+        doneSlave();
     }
 
     /**
@@ -52,6 +74,14 @@ public class MasterSlaveBarrier {
      * interrupt current thread
      */
     public void doneSlave() {
-        Thread.currentThread().interrupt();
+        current.interrupt();
+    }
+
+    public Thread getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Thread current) {
+        this.current = current;
     }
 }
