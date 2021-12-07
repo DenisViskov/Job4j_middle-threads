56dda66b09caf67e334ac00753127a2f3bacd768
Denis.Viskov <Denis.Viskov@nordigy.ru>
2021-12-07 10:29:48 +0300

    some

--- src/main/java/completablefuture/RowColSum.java
+++ src/main/java/completablefuture/RowColSum.java
@@ -1,9 +1,23 @@
 package completablefuture;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class RowColSum {
 
     public static Sums[] sum(final int[][] matrix) {
-        return null;
+        Sums[] result = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            final int currIndex = i;
+            Sums sums = new Sums(
+                Arrays.stream(matrix[i]).sum(),
+                Arrays.stream(matrix).mapToInt(array -> array[currIndex]).sum()
+            );
+            result[currIndex] = sums;
+        }
+        return result;
     }
 
     public static Sums[] asyncSum(final int[][] matrix) {
