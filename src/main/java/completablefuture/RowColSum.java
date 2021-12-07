5eaaf7cf3e4b960d960a86be2371bbead096a8d8
Denis.Viskov <Denis.Viskov@nordigy.ru>
2021-12-07 09:27:14 +0300

    preparation step

---
+++ src/main/java/completablefuture/RowColSum.java
@@ -0,0 +1,12 @@
+package completablefuture;
+
+public class RowColSum {
+
+    public static Sums[] sum(final int[][] matrix) {
+        return null;
+    }
+
+    public static Sums[] asyncSum(final int[][] matrix) {
+        return null;
+    }
+}
---
+++ src/main/java/completablefuture/Sums.java
@@ -0,0 +1,9 @@
+package completablefuture;
+
+import lombok.Data;
+
+@Data
+public class Sums {
+    private final int rowSum;
+    private final int colSum;
+}
