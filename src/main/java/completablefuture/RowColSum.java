ad0cff91a0735f60672a344e54aa684ce599dcdd
Denis.Viskov <Denis.Viskov@nordigy.ru>
2021-12-07 11:14:03 +0300

    some

--- src/main/java/completablefuture/RowColSum.java
+++ src/main/java/completablefuture/RowColSum.java
@@ -1,14 +1,16 @@
 package completablefuture;
 
+import lombok.SneakyThrows;
+
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class RowColSum {
 
     public static Sums[] sum(final int[][] matrix) {
-        Sums[] result = new Sums[matrix.length];
+        final Sums[] result = new Sums[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             final int currIndex = i;
             Sums sums = new Sums(
@@ -20,7 +22,26 @@ public class RowColSum {
         return result;
     }
 
+    @SneakyThrows
     public static Sums[] asyncSum(final int[][] matrix) {
-        return null;
+        final Sums[] result = new Sums[matrix.length];
+        final Map<Integer, CompletableFuture<Sums>> map = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            map.put(i, calculate(matrix, i));
+        }
+        for (int key : map.keySet()) {
+            result[key] = map.get(key).get();
+        }
+        return result;
+    }
+
+    private static CompletableFuture<Sums> calculate(final int[][] matrix, final int index) {
+        return CompletableFuture.supplyAsync(() -> {
+            Sums sums = new Sums(
+                Arrays.stream(matrix[index]).sum(),
+                Arrays.stream(matrix).mapToInt(array -> array[index]).sum()
+            );
+            return sums;
+        });
     }
 }
